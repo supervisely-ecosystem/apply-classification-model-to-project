b104cf2a21d8989abea4df44b9bbe6fdb0971705
Nikita <arzamaznik@gmail.com>
2023-01-12 19:54:57 +0600

    widget_id provided to widgets

--- src/connect_to_model/widgets.py
+++ src/connect_to_model/widgets.py
@@ -3,13 +3,13 @@ from supervisely.app.widgets import ProjectSelector, ElementButton, SlyTqdm, Don
 
 import src.sly_globals as g
 
-connect_model_button = ElementButton(text='CONNECT', button_type='primary')
-reselect_model_button = ElementButton(text='<i style="margin-right: 5px" class="zmdi zmdi-rotate-left"></i>connect to another model', button_type='warning', button_size='small', plain=True)
-model_selected_done_label = DoneLabel(text='successfully connected')
+connect_model_button = ElementButton(text='CONNECT', button_type='primary', widget_id="connect_model_button")
+reselect_model_button = ElementButton(text='<i style="margin-right: 5px" class="zmdi zmdi-rotate-left"></i>connect to another model', button_type='warning', button_size='small', plain=True, widget_id="reselect_model_button")
+model_selected_done_label = DoneLabel(text='successfully connected', widget_id="model_selected_done_label")
 
-toggle_all_previews_button = ElementButton(text='show all', plain=True, button_size='small')
+toggle_all_previews_button = ElementButton(text='show all', plain=True, button_size='small', widget_id="toggle_all_previews_button")
 
 
-connect_model_notification_box = NotificationBox(description='Select served Classification model from list below')
-model_classes_notification_box = NotificationBox(title='Model available Classes', description='images preview of the classes the model predicts')
-model_info_notification_box = NotificationBox(description='Model Info')
+connect_model_notification_box = NotificationBox(description='Select served Classification model from list below', widget_id="connect_model_notification_box")
+model_classes_notification_box = NotificationBox(title='Model available Classes', description='images preview of the classes the model predicts', widget_id="model_classes_notification_box")
+model_info_notification_box = NotificationBox(description='Model Info', widget_id="model_info_notification_box")
--- src/preferences/widgets.py
+++ src/preferences/widgets.py
@@ -3,18 +3,18 @@ from supervisely.app.widgets import ProjectSelector, ElementButton, SlyTqdm, Don
 
 import src.sly_globals as g
 
-select_preferences_button = ElementButton(text='confirm preferences')
-reselect_preferences_button = ElementButton(text='<i style="margin-right: 5px" class="zmdi zmdi-rotate-left"></i>reselect preferences', button_type='warning', button_size='small', plain=True)
+select_preferences_button = ElementButton(text='confirm preferences', widget_id="select_preferences_button")
+reselect_preferences_button = ElementButton(text='<i style="margin-right: 5px" class="zmdi zmdi-rotate-left"></i>reselect preferences', button_type='warning', button_size='small', plain=True, widget_id="reselect_preferences_button")
 
-preview_results_button = ElementButton(text='<i class="zmdi zmdi-collection-image" style="margin-right: 5px"></i> preview results', button_type='warning', button_size='small')
+preview_results_button = ElementButton(text='<i class="zmdi zmdi-collection-image" style="margin-right: 5px"></i> preview results', button_type='warning', button_size='small', widget_id="preview_results_button")
 preview_results_button.disabled = True
 
-preview_grid_gallery = GridGallery(columns_number=3, show_opacity_slider=False, sync_views=True)
+preview_grid_gallery = GridGallery(columns_number=3, show_opacity_slider=False, sync_views=True, widget_id="preview_grid_gallery")
 
-labeling_progress = SlyTqdm()
-labeling_done_label = DoneLabel(text='done')
+labeling_progress = SlyTqdm(widget_id="labeling_progress")
+labeling_done_label = DoneLabel(text='done', widget_id="")
 
-select_all_classes_button = ElementButton(text='<i class="zmdi zmdi-check-all"></i> SELECT ALL', button_type='text', button_size='mini', plain=True)
-deselect_all_classes_button = ElementButton(text='<i class="zmdi zmdi-square-o"></i> DESELECT ALL', button_type='text', button_size='mini', plain=True)
+select_all_classes_button = ElementButton(text='<i class="zmdi zmdi-check-all"></i> SELECT ALL', button_type='text', button_size='mini', plain=True, widget_id="select_all_classes_button")
+deselect_all_classes_button = ElementButton(text='<i class="zmdi zmdi-square-o"></i> DESELECT ALL', button_type='text', button_size='mini', plain=True, widget_id="deselect_all_classes_button")
 
-start_labeling_button = ElementButton(text='Apply Classifier to Images Project')
+start_labeling_button = ElementButton(text='Apply Classifier to Images Project', widget_id="start_labeling_button")
